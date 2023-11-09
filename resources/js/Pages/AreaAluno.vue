fc22f12ec1aa7efc6748bbe3fd4f6bc6bf5ee054
Matheus <matheustrilhakoch@hotmail.com>
2023-11-08 21:39:09 -0300

    Criando Area do Aluno

---
+++ resources/js/Pages/AreaAluno.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <p>Area do Aluno</p>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/aluno', function() {
+        return Inertia::render('AreaAluno');
+    });
 });
 
 require __DIR__.'/auth.php';

e65caed5927e0c6a00f1a23db3ed42cac6e5b2c2
Matheus <matheustrilhakoch@hotmail.com>
2023-11-10 21:27:00 -0300

    Removendo area do aluno para ajuste

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/aluno', function() {
-        return Inertia::render('AreaAluno');
-    });
 });
 
 require __DIR__.'/auth.php';
